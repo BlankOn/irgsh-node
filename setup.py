a4ece80dfe990ea2d36e521861cc9e88fdde7e5f
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-03 17:38:26 +0100

    Add entry point definition

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+def get_version():
+    import irgsh_node
+    return irgsh_node.__version__
+
+setup(name='irgsh-node', 
+      version=get_version(),
+      description='irgsh builder',
+      url='http://irgsh.blankonlinux.or.id',
+      packages=['irgsh_node'],
+      maintainer='BlankOn Developers',
+      maintainer_email='blankon-dev@googlegroups.com',
+      entry_points={'console_scripts': ['irgsh-node = irgsh_node.main:main']},
+     )
