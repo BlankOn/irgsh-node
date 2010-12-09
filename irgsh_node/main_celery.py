27d197fb2251c7ce53f1c27665f1609fce366845
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-09 13:00:31 +0100

    Force AMQP connection to use configured SSL certificate

--- irgsh_node/main_celery.py
+++ irgsh_node/main_celery.py
@@ -1,6 +1,9 @@
 import os
 
 def main():
+    import amqplib_sslcert_patch
+    amqplib_sslcert_patch.use_configured_cert()
+
     os.environ.setdefault('IRGSH_NODE_COFNIG', 'irgsh-node.conf')
     os.environ.setdefault('CELERY_LOADER', 'irgsh_node.loader.IrgshNodeLoader')
 
