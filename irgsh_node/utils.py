ccbd2eae4b1ce9d945467f4ebf8e56522b500a30
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-09 17:46:06 +0100

    Add function to send message to manager

---
+++ irgsh_node/utils.py
@@ -0,0 +1,17 @@
+import urllib
+
+from .conf import settings
+
+def send_message(url, param=None):
+    opts = {}
+    if settings.SSL_CERT is not None:
+        opts['cert_file'] = settings.SSL_CERT
+    if settings.SSL_KEY is not None:
+        opts['key_file'] = settings.SSL_KEY
+
+    data = None
+    if param is not None:
+        data = urllib.urlencode(param)
+    opener = urllib.URLopener(**opts)
+    opener.open(url, data)
+
