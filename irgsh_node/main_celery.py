c569fdef2ce9730f369da10db37b8699bb14da29
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-09 16:34:44 +0100

    Fix typo

--- irgsh_node/main_celery.py
+++ irgsh_node/main_celery.py
@@ -4,7 +4,7 @@ def main():
     import amqplib_sslcert_patch
     amqplib_sslcert_patch.use_configured_cert()
 
-    os.environ.setdefault('IRGSH_NODE_COFNIG', 'irgsh-node.conf')
+    os.environ.setdefault('IRGSH_NODE_CONFIG', 'irgsh-node.conf')
     os.environ.setdefault('CELERY_LOADER', 'irgsh_node.loader.IrgshNodeLoader')
 
     from celery.bin import celeryd
