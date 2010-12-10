8bb33f928ff9e9939aaf0ed2e8c7127c2b5fb875
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-10 10:16:12 +0100

    Update models: add content type and rename is absolute path field

--- irgsh_node/models.py
+++ irgsh_node/models.py
@@ -9,6 +9,9 @@ from irgsh_node.conf import settings
 
 Base = declarative_base()
 
+CT_RESULT = 0
+CT_LOG = 1
+
 class UploadQueue(Base):
     __tablename__ = 'irgsh_uploadqueue'
     __table_args__ = {'sqlite_autoincrement': True}
@@ -17,13 +20,15 @@ class UploadQueue(Base):
                    primary_key=True,
                    autoincrement=True)
     task_id = sa.Column(sa.String(255))
+
+    content_type = sa.Column(sa.Integer, default=CT_RESULT)
+    path = sa.Column(sa.String(255))
+    is_absolute_path = sa.Column(sa.Boolean, default=False)
+
     created = sa.Column(sa.DateTime, default=datetime.now, nullable=True)
     updated = sa.Column(sa.DateTime, default=datetime.now,
                         onupdate=datetime.now, nullable=True)
     uploaded = sa.Column(sa.DateTime, default=None, nullable=True)
-
-    filename = sa.Column(sa.String(255))
-    absolute_path = sa.Column(sa.Boolean, default=False)
 
 _session = None
 def get_session():
