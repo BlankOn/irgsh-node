27fccc0d1772fa696a124d22113aa67a511b4c62
Fajran Iman Rusadi <fajran@gmail.com>
2010-12-31 15:22:42 +0100

    Add function to get spec status

--- irgsh_node/manager.py
+++ irgsh_node/manager.py
@@ -1,4 +1,5 @@
 import urllib2
+import json
 
 from .conf import settings
 from .utils import send_message
@@ -17,6 +18,7 @@ FINISHED = 999
 URL_UPDATE_STATUS = '%(host)s/build/%(task_id)s/status/'
 URL_BUILD_LOG = '%(host)s/build/%(task_id)s/log/'
 URL_CONTROL = '%(host)s/build/%(task_id)s/info/'
+URL_GET_STATUS = '%(host)s/build/%(spec_id)s/status/'
 
 def update_status(task_id, status):
     host = settings.SERVER.rstrip('/')
@@ -41,4 +43,10 @@ def send_control(task_id, fname):
     param = {'control': open(fname, 'rb'),
              'builder': settings.NODE_NAME}
     send_message(url, param)
+
+def get_spec_status(spec_id):
+    host = settings.SERVER.rstrip('/')
+    url = URL_GET_STATUS = {'host': host, 'spec_id': spec_id}
+
+    return json.loads(send_message(url))
 
