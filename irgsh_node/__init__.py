9a95f541a7b7ee1728218f1cf9e5339fed911ae7
Fajran Iman Rusadi <fajran@gmail.com>
2011-01-17 21:15:52 +0100

    Setup logging

--- irgsh_node/__init__.py
+++ irgsh_node/__init__.py
@@ -1,2 +1,15 @@
 __version__ = '0.1'
 
+import logging
+
+def setup_logging():
+    log = logging.getLogger('irgsh_node')
+    log.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+
+setup_logging()
+
