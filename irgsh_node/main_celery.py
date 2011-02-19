6cfe294e9aebf1288a36e4ac99169664b7b8f40b
Fajran Iman Rusadi <fajran@gmail.com>
2011-02-19 22:34:46 +0100

    Use configured node name as the default hostname

--- irgsh_node/main_celery.py
+++ irgsh_node/main_celery.py
@@ -5,7 +5,20 @@ def main():
     os.environ.setdefault('CELERY_LOADER', 'irgsh_node.loader.IrgshNodeLoader')
 
     from celery.bin import celeryd
-    celeryd.main()
+    from celery.bin.celeryd import multiprocessing
+    from irgsh_node.conf import settings
+
+    class WorkerCommand(celeryd.WorkerCommand):
+        def get_options(self):
+            opts = super(WorkerCommand, self).get_options()
+            for opt in opts:
+                if opt.dest == 'hostname':
+                    opt.default = settings.NODE_NAME
+            return opts
+
+    multiprocessing.freeze_support()
+    worker = WorkerCommand()
+    worker.execute_from_commandline()
 
 if __name__ == '__main__':
     main()
