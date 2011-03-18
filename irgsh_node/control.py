eb11c1dc19c14c4bd1d1a8ce0584215bf7b24a78
Fajran Iman Rusadi <fajran@gmail.com>
2011-03-18 14:20:29 +0100

    Remove ping time limit

--- irgsh_node/control.py
+++ irgsh_node/control.py
@@ -1,20 +1,10 @@
-from datetime import datetime, timedelta
-
 from celery.worker.control import Panel
 
 from . import manager
 
-_ping_threshold = timedelta(minutes=5)
-_last_ping = None
-
 @Panel.register
 def report_alive(panel):
-    global _last_ping
-
-    now = datetime.now()
-    if _last_ping is None or now - _last_ping >= _ping_threshold:
-        _last_ping = now
-        manager.ping()
+    manager.ping()
 
     return {'status': 'ok'}
 
